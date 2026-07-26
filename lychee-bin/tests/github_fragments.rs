c56e19a55142aec20151b5d688f8c9c06fe7fc71
rina <k@rina.fyi>
2026-07-26 14:52:10 +1000

    refactor into extract_special_casing_html and add comment

--- lychee-bin/tests/github_fragments.rs
+++ lychee-bin/tests/github_fragments.rs
@@ -1,21 +1,30 @@
 #[cfg(test)]
 mod github_fragments {
+    use lychee_lib::extract::fragments::generate_without_disambiguation;
 
-    fn test_special_casing_html(html: &str) {
-        for l in html.lines() {
-            if l.contains("class=\"markdown-heading\"") {
-                let title = l.split_once("</h1>").unwrap().0.rsplit_once('>').unwrap().1;
-                let expected = l
-                    .split_once("id=\"user-content-")
-                    .unwrap()
-                    .1
-                    .split_once('"')
-                    .unwrap()
-                    .0;
-                let actual = lychee_lib::extract::fragments::generate_without_disambiguation(title);
-                assert_eq!(expected, actual);
-            }
+    /// Given HTML rendered by the Github API, this will extract
+    /// `(title, expected_fragment)` pairs.
+    ///
+    /// The HTML input has this format for Markdown fragment headings
+    /// (line breaks added for clarity and are not in the original HTML).
+    ///
+    /// ```html
+    /// <div class="markdown-heading" dir="auto">
+    ///   <h1 class="heading-element" dir="auto">ß-ß-Ss-SS- # LATIN SMALL LETTER SHARP S</h1>
+    ///   <a id="..." class="anchor" aria-label="..." href="#ß-ß-ss-ss---latin-small-letter-sharp-s">
+    ///   ...
+    /// ```
+    fn extract_special_casing_html(html: &str) -> impl std::iter::Iterator<Item = (&str, &str)> {
+        fn extract_line(l: &str) -> (&str, &str) {
+            let title = l.split_once("</h1>").unwrap().0.rsplit_once('>').unwrap().1;
+            let href = "href=\"";
+            let id = l.split_once(href).unwrap().1.split_once('"').unwrap().0;
+            (title, id)
         }
+
+        html.lines()
+            .filter(|l| l.contains("class=\"markdown-heading\""))
+            .map(extract_line)
     }
 
     /// Tests by using the Github API to render the HTML of a Markdown file
@@ -47,7 +56,9 @@ mod github_fragments {
 
         match resp.and_then(reqwest::Response::error_for_status) {
             Ok(resp) => {
-                test_special_casing_html(&resp.text().await?);
+                extract_special_casing_html(&resp.text().await?).for_each(|(title, expected)| {
+                    assert_eq!(expected, generate_without_disambiguation(title));
+                });
                 Ok(())
             }
             Err(err)
